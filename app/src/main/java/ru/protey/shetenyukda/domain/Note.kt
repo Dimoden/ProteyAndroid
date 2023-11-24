3998780364676d20807ad37218659e57872bb2da
Dimoden <sheteneyuk3mail@mail.ru>
2023-11-24 16:27:25 +0300

    Renamed

---
+++ app/src/main/java/ru/protey/shetenyukda/domain/Note.kt
@@ -0,0 +1,11 @@
+package ru.protey.shetenyukda.domain
+
+class Note {
+    var title: String
+    var text: String
+
+    constructor(title: String, text: String) {
+        this.title = title
+        this.text = text
+    }
+}
