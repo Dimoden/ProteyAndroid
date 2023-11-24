5e0f68e26046180e953e48aa0dfc3db7b5ca5770
Dimoden <sheteneyuk3mail@mail.ru>
2023-11-24 14:01:31 +0300

    Initial commit

---
+++ app/src/main/java/ru/protey/ivanovii/domain/Note.kt
@@ -0,0 +1,11 @@
+package ru.protey.ivanovii.domain
+
+class Note {
+    var title: String
+    var text: String
+
+    constructor(title: String, text: String) {
+        this.title = title
+        this.text = text
+    }
+}
